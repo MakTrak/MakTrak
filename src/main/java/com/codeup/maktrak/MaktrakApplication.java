74cbd8d357316a5c5ce689fd5e52d817fe69ccfa
Po <po.grammer.lin@gmail.com>
2018-01-29 10:44:52 -0600

    Completed Site Setup

--- src/main/java/com/codeup/maktrak/MaktrakApplication.java
+++ src/main/java/com/codeup/maktrak/MaktrakApplication.java
@@ -2,11 +2,18 @@ package com.codeup.maktrak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MaktrakApplication {
+public class MaktrakApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaktrakApplication.class, args);
 	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MaktrakApplication.class);
+	}
+
 }
