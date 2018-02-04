a3b9bdb72cf6c17e524323a8fe449b8589c4f297
Tito Valiente <titolvaliente@gmail.com>
2018-02-04 15:19:29 -0600

    added js validation to forms

--- src/main/resources/static/js/init.js
+++ src/main/resources/static/js/init.js
@@ -4,6 +4,17 @@
         $('.button-collapse').sideNav();
         $('.parallax').parallax();
 
+        $(document).ready(function() {
+            $('select').material_select();
+
+            // for HTML5 "required" attribute
+            $("select[required]").css({
+                display: "inline",
+                height: 0,
+                padding: 0,
+                width: 0
+            });
+        });
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
